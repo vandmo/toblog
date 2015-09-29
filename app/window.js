8358aad48031fc5cedc9b6d4dc94fbe40f3bcec1
Mikael Vandmo <mikael.vandmo@wirelesscar.com>
2015-09-29 14:36:55 +0200

    progress

--- app/window.js
+++ app/window.js
@@ -1,20 +1,50 @@
+var selected = {};
+
 function addToMovieList(bloggedMovie) {
   var list = document.getElementById("movieList");
   var li = document.createElement("li");
-  li.appendChild(document.createTextNode(bloggedMovie.title));
+  var inp = document.createElement("input");
+  var ia = document.createAttribute("type");
+  ia.value = "checkbox";
+  inp.setAttributeNode(ia);
+  inp.addEventListener("click", function() {
+    if (inp.checked) {
+      selected[bloggedMovie.titleId] = bloggedMovie;
+    } else {
+      delete selected[bloggedMovie.titleId];
+    }
+  });
+  li.appendChild(inp);
+  li.appendChild(document.createTextNode(bloggedMovie.title+" - "+bloggedMovie.comment));
   list.appendChild(li);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+function update() {
   chrome.storage.sync.get("bloggedMovies", function(v) {
     if (!v.bloggedMovies) {
       v.bloggedMovies = {};
+    }
+    var l = document.getElementById("movieList");
+    while (l.firstChild) {
+      l.removeChild(l.firstChild);
     }
     for (var titleId in v.bloggedMovies) {
       addToMovieList(v.bloggedMovies[titleId]);
     }
   });
-  document.getElementById("saveButton").addEventListener("click", function() {
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  update();
+
+  document.getElementById("deleteButton").addEventListener("click", function() {
+    document.getElementById("reallyDeleteButton").className = "";
+  });
+  document.getElementById("reallyDeleteButton").addEventListener("click", function() {
+    chrome.storage.sync.clear(update);
+  });
+
+  document.getElementById("exportButton").addEventListener("click", function() {
     chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName: 'myfile.html'}, function(writableFileEntry) {
       writableFileEntry.createWriter(function(writer) {
         writer.write(new Blob(["fewfwe"], {type: 'text/plain'}));
